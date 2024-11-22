6df2fdb4a2381fca1d94596b5b4a01866e9dadcb
Silvanus Bordignon <silvanusbordignon@gmail.com>
2024-11-22 17:20:05 +0100

    Started implementing the Allreduce butterfly

--- collective/allreduce-butterfly/allreduce-butterfly.c
+++ collective/allreduce-butterfly/allreduce-butterfly.c
@@ -1,11 +1,59 @@
 /*
     Implement a custom Allreduce function that works the same as the one
     provided by MPI, but works by creating a butterfly network to handle
-    sharing the data and computing the sums
+    sharing the data and computing the sums.
+
+    Caveats:
+    - only works with a number of processors equal to a power of 2
+    - only works with the sum operation
 */
 
 #include <mpi.h>
 #include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+#include <math.h>
+
+// Custom implementation of the Allreduce function using only Send and Recv
+int MyMPI_Allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
+
+    // To keep using the same function signature I need to repeat these lines
+
+    int my_rank;
+    int comm_sz;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    // Actual implementation
+
+    int height, group_sz, group_leader_rank, source, target;
+    int local_sum = *((int *) sendbuf);
+
+    for (height = 0; height < log2(comm_sz); height++) {
+        
+        group_sz = (int) pow(2, height + 1);
+        group_leader_rank = my_rank - my_rank % group_sz;
+
+        source = // ???
+        target = group_leader_rank + ((my_rank + (int) pow(2, height)) % group_sz);
+
+        // First iteration: only send
+        if (height == 0) {
+            MPI_Send(&local_sum, 1, MPI_INT, target, 0, MPI_COMM_WORLD);
+        }
+        // Last iteration: only receive
+        else if (height = log2(comm_sz) - 1) {
+            // eh... MPI_Recv(&recvbuf, 1, MPI_INT, )
+        }
+        // Intermediate iterations: receive and send
+        else {
+
+        }
+    }
+}
 
 int main() {
 
@@ -20,8 +68,25 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
     // Generating the values we will sum together
+    
+    int value, sum;
 
-    // Calling our custom Allreduce function
+    srand(time(NULL) * my_rank);
+    value = rand() % 100;
+
+    printf("[%d] value: %d\n", my_rank, value);
+
+    // Calling the default Allreduce function
+
+    MPI_Allreduce(&value, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    printf("[%d] Default implementation: %d\n", my_rank, sum);
+
+    // Calling our own Allreduce function
+
+    if (comm_sz % 2 == 0) {
+        MyMPI_Allreduce(&value, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+        printf("[%d] Custom implementation: %d\n", my_rank, sum);
+    }
 
     // Tearing down the MPI structures
 
--- collective/allreduce-butterfly/allreduce-butterfly.sh
+++ collective/allreduce-butterfly/allreduce-butterfly.sh
@@ -1,6 +1,6 @@
-#PBS -l select=4:ncpus=4:mem=250mb
+#PBS -l select=16:ncpus=16:mem=256mb
 #PBS -l walltime=0:00:15
 #PBS -q short_cpuQ
 
 module load mpich-3.2
-mpirun.actual -np 16 ~/hpc4ds/collective/allreduce-butterfly/allreduce-butterfly
+mpirun.actual -np 256 ~/hpc4ds/collective/allreduce-butterfly/allreduce-butterfly
